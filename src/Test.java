c0e39b89ed3111cfb33a8d2868e298f96b1e2a45
Maximal51 <maxmalek@gmx.de>
2024-05-25 12:26:40 +0200

    test

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -5,7 +6,7 @@ import java.util.Collections;
 public class Test {
     public static void main(String[] args) {
 
-
+        System.out.println("Penis");
 
 
     }
