68571f593a91e8e95ebaa66dd1878f651ea84ed4
Maximal51 <maxmalek@gmx.de>
2024-05-28 11:26:07 +0200

    Wartungsverträge

--- src/Test.java
+++ src/Test.java
@@ -6,8 +6,5 @@ import java.util.Collections;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("Penis");
-
-
     }
 }
--- src/WartungsVerträge.java
+++ src/WartungsVerträge.java
@@ -1,6 +1,3 @@
 public class WartungsVerträge {
-    public static void main(String[] args) {
-        System.out.println("penis");
     }
 
-    }
