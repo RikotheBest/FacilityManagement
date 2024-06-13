58df9fdc89f4e192612444fe82c9cc7aadfd870d
Riyad Dashdamirli <riyad.dashdamirli@gmail.com>
2024-06-13 23:28:44 +0200

    JavaFX eingestellt

--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,25 @@
-import Attribute.Adresse;
-import Attribute.Groesse;
-import Ausstattung.Ausstattung_Organisator;
-import Gebaeude.Gebaeude_Organisator;
-import Kunden.Kunde_Organisator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.sql.SQLException;
-
-public class Test {
+public class Test extends Application {
     public static void main(String[] args) {
-        Kunde_Organisator k = new Kunde_Organisator();
-        k.add("HWR", new Gebaeude_Organisator(),1);
-        k.add("FU", new Gebaeude_Organisator(),2);
-        k.add("TU", new Gebaeude_Organisator(),3);
 
-        try {
-            k.uploadGebaeude();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(k.getGebaeudeListe(0));
-        System.out.println(k.getGebaeudeListe(1));
-        System.out.println(k.getGebaeudeListe(2));
 
+    launch(args);
+
+
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("FacilityManagement");
+        stage.setScene(new Scene(root,300,275));
+        stage.show();
 
     }
 }
