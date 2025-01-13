2644f87fac7f54fcec3dedf018df7f2bc22fda84
Riyad Dashdamirli <riyad.dashdamirli@gmail.com>
2025-01-13 17:17:08 +0100

    updated the module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,13 @@ module org.example {
     requires com.google.gson;
 
     opens org.example to javafx.fxml;
+    opens org.example.Auftraege to javafx.base;
+    opens org.example.Ausstattung to javafx.base;
     opens org.example.View to javafx.fxml;
     opens org.example.Attribute to com.google.gson;
+
+    exports org.example.Auftraege to javafx.base;
+    exports org.example.Ausstattung to javafx.base;
     exports org.example.Attribute to com.google.gson;
     exports org.example.View to javafx.fxml;
     exports org.example;
